70b5c427aa3e694efab9d4bfa793a91ed95a47c9
bryandev <brayan7br7@gmail.com>
2024-10-23 22:53:01 -0500

    fisrt commint

---
+++ src/main/java/com/project/projectbackculture/ProjectBackCultureApplication.java
@@ -0,0 +1,13 @@
+package com.project.projectbackculture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBackCultureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBackCultureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/projectbackculture/ProjectBackCultureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.projectbackculture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBackCultureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
