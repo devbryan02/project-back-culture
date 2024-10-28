f36b3da6b232e38538f6dabd7e7990bb95c01649
bryandev <brayan7br7@gmail.com>
2024-10-28 10:24:16 -0500

    create class ConnectionDatabase and Test

---
+++ src/main/java/com/project/projectbackculture/config/ConnectionDatabase.java
@@ -0,0 +1,37 @@
+package com.project.projectbackculture.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.util.logging.Logger;
+
+@Configuration
+public class ConnectionDatabase {
+
+    private final Logger log = Logger.getLogger(this.getClass().getName());
+
+    @Value("${database.url}")
+    private String databaseUrl;
+
+    @Value("${database.userName}")
+    private String databaseUsername;
+
+    @Value("${database.password}")
+    private String databasePassword;
+
+    @Bean
+    DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(this.databaseUrl);
+        dataSource.setUsername(this.databaseUsername);
+        dataSource.setPassword(this.databasePassword);
+        log.info("Conexion establecida");
+        return dataSource;
+    }
+
+}
+
