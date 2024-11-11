9c44ba85ad190d8c7adb2e48e10d53b67a7e939b
bryandev <brayan7br7@gmail.com>
2024-11-10 19:59:00 -0500

    move and remove method create user

--- src/main/java/com/project/projectbackculture/web/controller/UserController.java
+++ src/main/java/com/project/projectbackculture/web/controller/UserController.java
@@ -1,13 +1,6 @@
 package com.project.projectbackculture.web.controller;
 
 import com.project.projectbackculture.service.implement.UserServiceImpl;
-import com.project.projectbackculture.web.request.NewUserRequest;
-import com.project.projectbackculture.web.response.UserResponse;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,13 +13,6 @@ public class UserController {
     public UserController(UserServiceImpl userService) {
         this.userService = userService;
     }
-
-    @PostMapping
-    public ResponseEntity<UserResponse> createUser(@RequestBody @Valid NewUserRequest newUserRequest) {
-        UserResponse userResponse = userService.save(newUserRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
-    }
-
 
 }
 
