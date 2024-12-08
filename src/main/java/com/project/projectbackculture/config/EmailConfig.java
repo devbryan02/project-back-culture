a3955e923ea30957863f81f10fe746807e1f4703
bryacmy <brayan7br7@gmail.com>
2024-12-07 20:27:37 -0500

    config javaEmail

---
+++ src/main/java/com/project/projectbackculture/config/EmailConfig.java
@@ -0,0 +1,40 @@
+package com.project.projectbackculture.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+
+    @Value("${google.email}")
+    private String EMAIL_USER;
+
+    @Value("${google.password}")
+    private  String EMAIL_PASSWORD;
+
+
+    @Bean
+    public JavaMailSender javaMailSender(){
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(EMAIL_USER);
+        mailSender.setPassword(EMAIL_PASSWORD);
+
+        Properties properties = mailSender.getJavaMailProperties();
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+}
