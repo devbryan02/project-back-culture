7fb975ebc937f7e38f0cfee15cd73bc3469e5d16
bryacmy <brayan7br7@gmail.com>
2024-12-10 10:34:11 -0500

    configure jackson

---
+++ src/main/java/com/project/projectbackculture/config/JacksonConfig.java
@@ -0,0 +1,19 @@
+package com.project.projectbackculture.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Locale;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setLocale(Locale.US);
+        return mapper;
+    }
+
+}
